f9b19a7f4db5d01142d2bd46c0e384b9c09bb0f6
alexdotwells <ajwells99@gmail.com>
2018-01-18 21:29:51 -0600

    initial commit

--- react-webpack-babel/README.md
+++ react-webpack-babel/README.md
@@ -1,1 +1,39 @@
-# react-webpack-babel
+# React Starter
+
+A basic template that consists of the essential elements that are required to start building a React application.
+
+
+### Install
+
+* Install node modules
+
+   ```bash
+   npm install
+   npm dedupe
+   ```
+
+### Build
+
+* Build application
+
+  This command will also run ESLint as part of build process.
+
+  ```bash
+  npm run build
+  ```
+
+### Run
+
+* Run start
+
+  This will run the _'serve'_ npm task
+
+  ```bash
+  npm start
+  ```
+
+* Run webpack dev server (simple development http server with live reload capability)
+
+  ```bash
+  npm run serve:dev
+  ```
---
+++ react-webpack-babel/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const CleanWebPackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.svg|.png|.jpg$/,
+                loader: 'url-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebPackPlugin([ 'public' ], { root: path.resolve(__dirname)}),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            favicon: './src/favicon.ico',
+            inject: false
+        })
+    ],
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public'),
+        compress: true,
+        port: 9000
+    }
+};
